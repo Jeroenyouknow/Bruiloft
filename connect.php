3634ac32ba191675c84c6d112feb41051260c15a
Jeroen Huiskes <jeroen-h@hotmail.nl>
2017-08-29 21:06:25 +0200

    Inital commit

---
+++ connect.php
@@ -0,0 +1,14 @@
+<?php
+
+$servername = "rdbms.strato.de";
+$username = "U3084260";
+$dbpass = "";
+$dbname = "DB3084260";
+
+$err_level = error_reporting(0);
+$db = mysqli_connect($servername, $username, $dbpass, $dbname);
+if (!$db) {
+    echo("Connection failed");
+}
+error_reporting($err_level);
+?>
